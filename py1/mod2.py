70ec9d1b9a8f7e593f19c27c858ee1267bb63c0f
Jon Choate <choatej@gmail.com>
2024-09-15 18:09:22 -0400

    feat: initial commit

---
+++ py1/mod2.py
@@ -0,0 +1,6 @@
+
+def hello():
+    print("hello from mod2")
+
+if __name__ == "__main__":
+    hello()
---
+++ src/mod1.py
@@ -0,0 +1,6 @@
+
+def hello():
+    print("hello from mod1")
+
+if __name__ == "__main__":
+    hello()
